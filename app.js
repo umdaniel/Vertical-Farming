83260eb98063e17f192840399b40797dead22184
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-05-27 14:43:40 -0400

    Added more available products

--- app.js
+++ app.js
@@ -41,6 +41,22 @@ class Products {
 class UI {
     displayProducts(products) {
         let result = '';
+        products.forEach(product => {
+            result += `
+            <article class="product">
+            <div class="img-container">
+                <img src=${product.image} alt="product" class="product-img">
+                <button class="bag-btn" data-id=${product.id}>
+                    <i class="fas fa-shopping-cart"></i>
+                    add to bag
+                </button>
+            </div>
+            <h3>${product.title}</h3>
+            <h4>${product.price}</h4>
+        </article>
+            `;
+        });
+        productsDOM.innerHTML = result;
     }
 }
 
