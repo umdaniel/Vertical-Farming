d26638fb437eedb54bf242f7e6e3ea61056478f5
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-05-27 13:12:06 -0400

    Added JS functionality + changed JSON

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
 // All of the JavaScript logic.
+
+// Variable declarations.
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOveraly = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+// Cart.
+let cart = [];
+
+// Responsible for getting the products.
+class Products {
+    // Gets the products from the json file.
+    async getProducts() {
+        try {
+            let result = await fetch('products.json');
+            let data = await result.json();
+            let products = data.items;
+
+            // Iterates through every product in the list.
+            products = products.map(item => {
+                const {title, price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {title, price, id, image};
+            })
+            return products;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+// Responsible for displaying the products.
+class UI {
+    displayProducts(products) {
+        let result = '';
+    }
+}
+
+// Responsible for local storage.
+class Storage {
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const ui = new UI();
+    const products = new Products();
+
+    // Get all products.
+    products.getProducts().then(products => ui.displayProducts(products));
+});
